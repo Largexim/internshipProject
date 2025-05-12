1a3ae5b5e6940fd11743e9c8dea11e9943a6302f
Largexim <largexim@gmail.com>
2025-05-12 21:34:49 +0330

    feat: GameOver logic added. some lines of code in BoxCollisionController script changed.

--- Assets/Scripts/BoxCollisionController.cs
+++ Assets/Scripts/BoxCollisionController.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 
 public class BoxCollisionController : MonoBehaviour
 {
+    [SerializeField] private GameObject GOC;
     BoxCollider2D c2D;
+    bool collided = false;
 
     private void Awake()
     {
@@ -15,11 +17,18 @@ public class BoxCollisionController : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Ground"))
+        if (other.gameObject.CompareTag("Ground") && !collided)
             c2D.enabled = false;
-        else if (other.gameObject.CompareTag("Player"))
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
         {
-            //do
+            collided = true;
+            other.gameObject.SetActive(false);
+            GOC.GetComponent<GameOverController>().ShowAnimation();
         }
     }
 }
---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameOverController : MonoBehaviour
+{
+
+    public void ShowAnimation()
+    {
+        Animator animator = GetComponent<Animator>();
+        //here we will add animation for game over text
+    }
+}
