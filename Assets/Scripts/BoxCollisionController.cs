c46ec836268820aad9533d9de3c6dc5b03b94c52
Largexim <largexim@gmail.com>
2025-05-13 00:36:40 +0330

    feat: Box animation added. sprites and animation created and added.

--- Assets/Scripts/BoxCollisionController.cs
+++ Assets/Scripts/BoxCollisionController.cs
@@ -7,8 +7,15 @@ using UnityEngine;
 public class BoxCollisionController : MonoBehaviour
 {
     [SerializeField] private GameObject GOC;
+    [SerializeField] private Animator animator;
+    private SpriteRenderer spriteRenderer;
     BoxCollider2D c2D;
     bool collided = false;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Awake()
     {
@@ -18,8 +25,29 @@ public class BoxCollisionController : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground") && !collided)
+        {
             c2D.enabled = false;
-        
+            if (transform.position.z == -1)
+                spriteRenderer.flipY = false;
+            else
+                spriteRenderer.flipY = true;
+            
+            animator.SetBool("Flag", true);
+            
+        }
+        else if (other.gameObject.CompareTag("Box") && !collided)
+        {
+            c2D.enabled = false;
+            if (transform.position.z == -1)
+            {
+                spriteRenderer.flipY = false;
+                transform.position -= new Vector3(0, 0, 1);
+            }
+            else
+                spriteRenderer.flipY = true;
+            
+            animator.SetBool("Flag", true);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
--- Assets/Scripts/BoxGeneratorLogic.cs
+++ Assets/Scripts/BoxGeneratorLogic.cs
@@ -21,8 +21,8 @@ public class BoxGeneratorLogic : MonoBehaviour
             float gameSpeed = gameController.GetComponent<GameControllerLogic>().gameSpeed;
             float waitTime = 3/gameSpeed;
             float randomX = possibleValuesForPosition[UnityEngine.Random.Range(0, 3)];
-            float randomZ = possibleValuesForPosition[UnityEngine.Random.Range(3, 4)];
-            Vector3 pos = new Vector3(randomX, 8f, randomZ); 
+            float randomZ = possibleValuesForPosition[UnityEngine.Random.Range(3, 5)];
+            Vector3 pos = new Vector3(randomX, 6f, randomZ); 
             
             yield return new WaitForSeconds(waitTime);
             Instantiate(boxPrefab, pos, Quaternion.identity);
